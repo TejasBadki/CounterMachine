634fdfbbeb2b747643d43fe39c4eb7623f8a6330
Tejas Badki <tejasbadki@gmail.com>
2024-11-20 05:17:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const myCount = document.getElementById("count");
+const message = document.getElementById("message");
+let c = 0;
+
+function incr(event) {
+    event.preventDefault();
+    c++;
+    updateDisplay();
+}
+
+function decr(event) {
+    event.preventDefault();
+    if (c > 0) {
+        c--;
+        updateDisplay();
+    } else {
+        message.textContent = "The counter cannot go below 0.";
+        message.style.color = 'red';
+        
+    }
+}
+
+function reset(event) {
+    event.preventDefault(); 
+    c = 0;
+    updateDisplay();
+    message.textContent = "";
+}
+
+//  display even/odd message
+function updateDisplay() {
+    message.style.color = '';
+    myCount.textContent = c;
+    if (c > 0) {
+        message.textContent = c % 2 === 0 
+            ? "The number is positive and even." 
+            : "The number is positive and odd.";
+    } else {
+        message.textContent = ""; // Clear the message if not positive
+    }
+}
